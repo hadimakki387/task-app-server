16d75a251fa55fe40cae232ce045461ebc2663e9
hadimakki387 <hmakki387@gmail.com>
2023-08-13 12:52:34 +0300

    provided adding tasks feature

--- src/controller/tasksController.js
+++ src/controller/tasksController.js
@@ -5,7 +5,13 @@ const getTasks = async (req, res) => {
 };
 
 const addTask = async (req, res) => {
-  console.log(req.body)
+  try {
+    const task = new Task(req.body);
+    await task.save();
+    res.json({ message: "Task added" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 module.exports = { getTasks, addTask };
